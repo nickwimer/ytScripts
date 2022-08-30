36000753395c0bbe37e91071087dc6f22452a5cb
nickwimer <nicholas.wimer@gmail.com>
2022-08-30 11:56:17 -0700

    adding load dataseries to utilities

---
+++ ytscripts/utilities.py
@@ -0,0 +1,26 @@
+"""Utility routines used throughout ytscripts."""
+import fnmatch
+import os
+
+import yt
+
+
+def load_dataseries(datapath, pname=None, units_override=None):
+    """Loads the plt files based on method."""
+    if pname is not None:
+        load_list = [os.path.join(datapath, x) for x in pname]
+        ts = yt.DatasetSeries(load_list, units_override=units_override)
+        # Find the index based on location of the selected plot files
+        all_files = fnmatch.filter(sorted(os.listdir(datapath)), "plt?????")
+
+        index_list = []
+        for plt in pname:
+            index_list.append(all_files.index(plt))
+
+    else:
+        ts = yt.load(
+            os.path.join(datapath, "plt?????"),
+            units_override=units_override,
+        )
+
+    return ts
