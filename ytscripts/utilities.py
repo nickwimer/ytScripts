d049ad9b1764d7dd82b3dc2340d368ab548d7ac8
nickwimer <nicholas.wimer@gmail.com>
2022-09-16 10:48:11 -0700

    Add argparse classes and update all routines for added functionality (#5)
    * add ytargs for parsing common args across files
    
    * ytargs for data extraction class and integrate extract slices
    
    * updating makedir method
    
    * updating extract slices to use new class and slice in any direction
    
    * rearrange functions in ytargs classes
    
    * update extract slices and add in grid offset
    
    * update slice plot to new args class structure
    
    * update plot slices to accept new extracted data structure on any normal
    
    * adding plot box to plot_slices script
    
    * fix typo in makedirs
    
    * adding cmap and fbounds to plot_slices
    
    * update readme

--- ytscripts/utilities.py
+++ ytscripts/utilities.py
@@ -2,6 +2,7 @@
 import fnmatch
 import os
 
+import numpy as np
 import yt
 
 
@@ -38,6 +39,8 @@ def get_attributes(ds):
         "right_edge": ds.domain_right_edge,
         "max_level": ds.max_level,
         "length_unit": ds.length_unit,
+        "time_unit": ds.time_unit,
+        "width": ds.domain_width,
     }
 
     # Make commonly used attributes
@@ -54,3 +57,13 @@ def get_attributes(ds):
         }
     )
     return ds_dict
+
+
+def get_fig_aspect_ratio(xlen, ylen, base=5):
+    """Get the aspect ratio to fit the data."""
+
+    aspect_ratio = np.ceil(ylen / xlen)
+    fx = base * (0.5 + aspect_ratio)
+    fy = base
+
+    return fx, fy
